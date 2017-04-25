709680f5083c04eae66c5b65d01b04805b6ac7a4
gaurav22021996 <gauravmouryaweb@gmail.com>
2017-04-25 11:38:18 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,30 +1,25 @@
 <?php
-	session_start();
+
+require_once('includes/config.php');
+
+require_once('includes/database.class.php');
+
+$db= new database($pdo);
 ?>
-<html lang="en-US">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-        <title>Chat box</title>
-        <link rel="shortcut icon" href="http://www.stylpix.com/images/favicon.png">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		
-        <link rel='stylesheet' href='css/chatbox.css' type='text/css' media='all'/>
-		<link rel="stylesheet" href="css/animate.css">
-		 <link rel="stylesheet" href="css/bootstrap.min.css">
-		 <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
-    </head>
- 
-    <body data-spy="scroll">
-	
-		<?php include("php/chat_box.php"); ?>
-		
-		
-		<h1>Chat box API for the website ...</h1>
-		
-    </body>
-</html>
+<?php
+		$query=$pdo->query("SELECT query");
 
+		while($exec=$query->fetch(PDO::FETCH_OBJ))
 
-
-
+		{
+			$exec->column_name;
+		}
+		
+		$query=("INSERT query");
+		
+		$exec=$pdo->prepare($query);
+		if($exec->execute())
+		{
+			// if get executed
+		}
+?>
